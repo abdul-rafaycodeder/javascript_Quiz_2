a5fb38903b731aed6f4cc7461e03840563ce9df1
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-04-01 05:52:10 +0500

    coding

---
+++ script.js
@@ -0,0 +1,23 @@
+//javascript OOP object
+let obj = {
+    name: 'rafay',
+    age: 18
+}
+
+//javascript OOP class
+class Student {
+    constructor(name, age, id, clas, rollNumber) {
+        this.name = name,
+            this.age = age,
+            this.id = id,
+            this.clas = clas,
+            this.rollNumber = rollNumber
+    }
+
+    yaFunctionHai() {
+        console.log(`Student ka naam ya hai ${this.name} or Age hai ya ${this.age} )
+
+    }
+
+
+}
